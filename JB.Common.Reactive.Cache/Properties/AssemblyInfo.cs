1c355b67f5646e83d9c5285c0b2fe253ae871dba
Jörg Battermann <jb@joergbattermann.com>
2015-12-16 20:42:24 +0100

    (Re)added wrapper Cache project

---
+++ JB.Common.Reactive.Cache/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JB.Common.Reactive.Cache")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("JB.Common.Reactive.Cache")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("950b0468-d1c7-410a-910c-54afef706e87")]
