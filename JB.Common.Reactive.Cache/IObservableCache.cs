d11a5c2d5cc0d8fbdecb3e3b4028ded9dae10cb5
Jörg Battermann <jb@joergbattermann.com>
2015-12-19 19:37:04 +0100

    ...

---
+++ JB.Common.Reactive.Cache/IObservableCache.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IObservableCache.cs" company="Joerg Battermann">
+//   Copyright (c) 2015 Joerg Battermann. All rights reserved.
+// </copyright>
+// <author>Joerg Battermann</author>
+// <summary></summary>
+// -----------------------------------------------------------------------
+namespace JB.Reactive.Cache
+{
+    public interface IObservableCache
+    {
+         
+    }
+}
