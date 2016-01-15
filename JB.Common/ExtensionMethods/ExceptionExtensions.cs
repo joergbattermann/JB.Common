d84616423a5bb2a519f03199525d4c649bfae023
Jörg Battermann <jb@joergbattermann.com>
2016-01-15 21:43:06 +0100

    Working on bufferedasyncsubject

---
+++ JB.Common/ExtensionMethods/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExceptionExtensions.cs" company="Joerg Battermann">
+//   Copyright (c) 2016 Joerg Battermann. All rights reserved.
+// </copyright>
+// <author>Joerg Battermann</author>
+// <summary></summary>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace JB.ExtensionMethods
+{
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Throws the <paramref name="exception"/> if it isn't null.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        public static void ThrowIfNotNull(this Exception exception)
+        {
+            if (exception != null)
+            {
+                System.Runtime.ExceptionServices.ExceptionDispatchInfo.Capture(exception).Throw();
+            }
+        }
+    }
+}
